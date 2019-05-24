51248898a7d763d75415a264cb7d5152303f396e
xan86 <50671523+xan86@users.noreply.github.com>
2019-05-24 09:53:12 +0200

    Create wifiKeepalive.sh

---
+++ wifiKeepalive.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# chmod +x /path/wifiKeepalive.sh
+# nano /etc/crontab
+# */5 *   * * *   root    /path/wifiKeepalive.sh
+# force stop 
+# ifdown --force wlan0
+# and try
+
+
+# The IP for the server you wish to ping (the gateway ip is enough to test wifi)
+SERVER=$(/sbin/ip route | awk '/default/ { print $3 }')
+
+# Only send two pings, sending output to /dev/null
+ping -c2 ${SERVER} > /dev/null
+
+# If the return code from ping ($?) is not 0 (meaning there was an error)
+if [ $? != 0 ]
+then
+    # Restart the wireless interface
+    ifdown --force wlan0
+    ifup wlan0
+fi
