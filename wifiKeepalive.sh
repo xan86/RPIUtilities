b02d5e48b5bd009da2f24b9411b3a2e564973b47
xan86 <50671523+xan86@users.noreply.github.com>
2019-05-25 01:03:40 +0200

    Update wifiKeepalive.sh

--- wifiKeepalive.sh
+++ wifiKeepalive.sh
@@ -8,9 +8,12 @@
 # sudo ifconfig wlan0 down
 # and try
 
-#SERVER=$(/sbin/ip route | awk '/default/ { print $3 }')
-SERVER=8.8.8.8
+#SERVER=8.8.8.8
+#Let's do local ping and do not disturbe google
+SERVER=$(/sbin/ip route | awk '/default/ { print $3 }')
+echo "Server to ping" $server
 res=$(ping -c1 192.168.2.100 | grep 64)
+echo "result" $res
 
 if [ -z "$res" ]
 then
