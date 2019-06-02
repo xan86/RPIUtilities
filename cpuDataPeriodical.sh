1a2f62a65117a3c9d621b25081f37c7fb1cb5b06
xan86 <nicola.frascarelli@gmail.com>
2019-06-02 14:12:38 +0200

    executable

--- cpuDataPeriodical.sh
+++ cpuDataPeriodical.sh
@@ -4,6 +4,6 @@ while true; do
 	temp=$(/opt/vc/bin/vcgencmd measure_temp | cut -d'=' -f 2)
 	cpuvolt=$(vcgencmd measure_volts core | cut -d'=' -f 2)
 	ramvolt=$(vcgencmd measure_volts sdram_c | cut -d'=' -f 2)
-	echo CPU: $temp $cpuvolt RAM: $ramvolt
+	echo CPU $temp $cpuvolt RAM $ramvolt
 	sleep 1
 done
